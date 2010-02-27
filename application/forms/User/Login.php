7563de33c11ffe7a5cec66f1fe4b7027f34f7eca
Valentin Bora <valentinbora@valentin-boras-macbook-pro.local>
2010-02-27 15:52:07 +0200

    Added User login form

---
+++ application/forms/User/Login.php
@@ -0,0 +1,24 @@
+<?php
+
+class Gasestema_Form_User_Login extends Zend_Form
+{
+    public function init()
+    {
+        $email = $this->createElement('text', 'email')
+            ->setLabel('Email')
+            ->setRequired(true)
+            ->addFilter('StringTrim')
+            ->addValidator('NotEmpty');
+            
+        $password = $this->createElement('password', 'password')
+            ->setLabel('Password')
+            ->setRequired(true)
+            ->addFilter('StringTrim')
+            ->addValidator('NotEmpty');
+            
+        $submit = $this->createElement('submit', 'submit')
+            ->setLabel('Login');
+        
+        $this->addElements(array($email, $password, $submit));
+    }
+}
