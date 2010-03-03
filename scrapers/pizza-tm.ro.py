df92b773e44dc614dc288ac037ce0062d2426ded
me <me@spidery.(none)>
2010-03-03 23:21:56 +0200

    bugfix

--- scrapers/pizza-tm.ro.py
+++ scrapers/pizza-tm.ro.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 #import mechanize
 #from mechanize import Browser
 import urllib2
@@ -11,13 +12,40 @@ import sys
 import MySQLdb
 link = MySQLdb.connect (host = "localhost",
                            user = "gasestema",
-                           passwd = "anaaregasestema",
+                           passwd = "gasestema",
                            db = "gasestema_test",
                            charset = "utf8",
                            use_unicode = True
                            )     
 
+
+def translit(string):
+    translit = {
+            u"\u0102" : "A",
+            u"\u0103" : "a",
+            u"\u00c2" : "A",
+            u"\u00e2" : "a",
+            u"\u00ce" : "I",
+            u"\u00ee" : "i",
+            u"\u0218" : "S",
+            u"\u0219" : "s",
+            u"\u021a" : "T",
+            u"\u021b" : "t",
+            u"\u015e" : "S",
+            u"\u015f" : "s",
+            u"\u0162" : "T",
+            u"\u0163" : "t"
+    }
+
+    for k in translit:
+        string = string.replace(k,translit[k])
+            
+    return string
+            
+
 def insertLabel(label):
+    label = translit(label)
+    
     cursor = link.cursor()
     result = cursor.execute("SELECT id FROM tag WHERE nume='"+str(label)+"' ")
     row = cursor.fetchone()
@@ -31,6 +59,13 @@ def insertLabel(label):
         return row[0]
     
 def insertLocatie(nume,localitate=1,adresa='',url='',contact='',descriere='',orar=''):
+    nume = translit(nume)
+    adresa = translit(adresa)
+    url = translit(url)
+    contact = translit(contact)
+    descriere = translit(descriere)
+    orar = translit(orar)
+    
     cursor = link.cursor()
     result = cursor.execute("SELECT id FROM locatie WHERE nume='"+str(nume)+"' ")
     row = cursor.fetchone()
@@ -45,7 +80,10 @@ def insertLocatie(nume,localitate=1,adresa='',url='',contact='',descriere='',ora
         return row[0]
         
 def insertNume(nume,descriere):
-    descriere = descriere
+    descriere = translit(descriere).replace("-","").strip()
+    nume = translit(nume).strip()
+    
+    
     cursor = link.cursor()
     result = cursor.execute("SELECT id FROM obiect_nume WHERE nume='"+str(nume)+"' and descriere='"+str(descriere)+"' ")
     row = cursor.fetchone()
@@ -60,8 +98,14 @@ def insertNume(nume,descriere):
         return row[0]
 
 def insertTagObiect(tagId,obiectId):
-    link.cursor().execute("set autocommit=1;INSERT INTO tag_obiect(tag,obiect) VALUES('"+str(tagId)+"','"+str(obiectId)+"')")
     cursor = link.cursor()
+    result = cursor.execute("SELECT id FROM tag_obiect WHERE tag='"+str(tagId)+"' and obiect='"+str(obiectId)+"' ")
+    row = cursor.fetchone()
+    if row:
+        return row[0]
+    else:
+        link.cursor().execute("set autocommit=1;INSERT INTO tag_obiect(tag,obiect) VALUES('"+str(tagId)+"','"+str(obiectId)+"')")
+        cursor = link.cursor()
 
 def insertObiect(numeId=0,localitate = 1,locatieId=0,user = 2,adaugat = False):
     if adaugat == False:
@@ -85,7 +129,7 @@ class crawler:
     
     
     def getUrl(self,url):
-        user_agent='Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.8.1.15) Gecko/20080623 Firefox/2.0.0.15'
+        user_agent='Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.8.1.15) Gecko/20080623'
         http_accept="text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8"
         request = urllib2.Request(url)
         request.add_header('User-Agent',user_agent)
@@ -123,9 +167,9 @@ for result in results:
         if type == 'telefon':
             telefon = value
         if type == 'adresa':
-            telefon = value
+            adresa = value
         if type == 'orar':
-            telefon = value            
+            orar = value            
     
     locatieId = insertLocatie(numePizzerie,1,adresa,site,telefon,"",orar);
     
